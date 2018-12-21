07bf5cfe02bcdf6465794edbf785a3ac0c44f248
Rui Meira <ruimiguelcm96@gmail.com>
2018-12-21 19:05:45 +0000

    a new commit

---
+++ EG/Controllers/Utilizador_PerfilController.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BD;
+
+namespace EG.Controllers
+{
+    public class Utilizador_PerfilController : Controller
+    {
+        private static estp2Entities db = new estp2Entities();
+
+        // GET: Utilizador_Perfil
+        public ActionResult Index()
+        {
+            var utilizador_Perfil = db.Utilizador_Perfil.Include(u => u.Perfil).Include(u => u.Utilizador);
+            return View(utilizador_Perfil.ToList());
+        }
+
+        // GET: Utilizador_Perfil/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Utilizador_Perfil utilizador_Perfil = db.Utilizador_Perfil.Find(id);
+            if (utilizador_Perfil == null)
+            {
+                return HttpNotFound();
+            }
+            return View(utilizador_Perfil);
+        }
+
+        // GET: Utilizador_Perfil/Create
+        public ActionResult Create()
+        {
+            ViewBag.perfil_id = new SelectList(db.Perfil, "id_user", "tipo_utilizador");
+            ViewBag.user_id = new SelectList(db.Utilizador, "id", "cod_postal");
+            return View();
+        }
+
+        public static bool adiciona(int user_id, int perfil_id)
+        {
+            Utilizador_Perfil u = new Utilizador_Perfil();
+            u.user_id = user_id;
+            u.perfil_id = perfil_id;
+            u.data = DateTime.Now;
+            u.ativo = 1;
+            db.Utilizador_Perfil.Add(u);
+            db.SaveChanges();
+            return true;
+        }
+
+        // POST: Utilizador_Perfil/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "user_id,perfil_id,data,ativo")] Utilizador_Perfil utilizador_Perfil)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Utilizador_Perfil.Add(utilizador_Perfil);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.perfil_id = new SelectList(db.Perfil, "id_user", "tipo_utilizador", utilizador_Perfil.perfil_id);
+            ViewBag.user_id = new SelectList(db.Utilizador, "id", "cod_postal", utilizador_Perfil.user_id);
+            return View(utilizador_Perfil);
+        }
+
+        // GET: Utilizador_Perfil/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Utilizador_Perfil utilizador_Perfil = db.Utilizador_Perfil.Find(id);
+            if (utilizador_Perfil == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.perfil_id = new SelectList(db.Perfil, "id_user", "tipo_utilizador", utilizador_Perfil.perfil_id);
+            ViewBag.user_id = new SelectList(db.Utilizador, "id", "cod_postal", utilizador_Perfil.user_id);
+            return View(utilizador_Perfil);
+        }
+
+        // POST: Utilizador_Perfil/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "user_id,perfil_id,data,ativo")] Utilizador_Perfil utilizador_Perfil)
+        {
+            if (ModelState.IsValid)
+            {
+                //db.Entry(utilizador_Perfil).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.perfil_id = new SelectList(db.Perfil, "id_user", "tipo_utilizador", utilizador_Perfil.perfil_id);
+            ViewBag.user_id = new SelectList(db.Utilizador, "id", "cod_postal", utilizador_Perfil.user_id);
+            return View(utilizador_Perfil);
+        }
+
+        // GET: Utilizador_Perfil/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Utilizador_Perfil utilizador_Perfil = db.Utilizador_Perfil.Find(id);
+            if (utilizador_Perfil == null)
+            {
+                return HttpNotFound();
+            }
+            return View(utilizador_Perfil);
+        }
+
+        // POST: Utilizador_Perfil/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Utilizador_Perfil utilizador_Perfil = db.Utilizador_Perfil.Find(id);
+            db.Utilizador_Perfil.Remove(utilizador_Perfil);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}

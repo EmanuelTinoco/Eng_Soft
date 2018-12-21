d8e66dceabdb4ac950fa6dfa3292321b6e528950
Rui Meira <ruimiguelcm96@gmail.com>
2018-12-21 21:57:00 +0000

    more one

---
+++ EG/Controllers/SugestaosController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using IdentidadeManager;
+using BD;
+
+namespace EG.Controllers
+{
+    public class SugestaosController : Controller
+    {
+        private estp2Entities db = new estp2Entities();
+
+        // GET: Sugestaos
+        public ActionResult Index()
+        {
+            var sugestao = db.Sugestao.Include(s => s.Utilizador);
+            return View(sugestao.ToList());
+        }
+
+        // GET: Sugestaos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sugestao sugestao = db.Sugestao.Find(id);
+            if (sugestao == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sugestao);
+        }
+
+        // GET: Sugestaos/Create
+        public ActionResult Create(int ?id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Utilizador utilizador = db.Utilizador.Find(id);
+            if (utilizador == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.id_user = new SelectList(db.Utilizador, "id", "id", utilizador.id);
+            return View();
+        }
+
+        // POST: Sugestaos/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "descricao")] Sugestao sugestao)
+        {
+            sugestao.id_user = int.Parse(IdentidadeUser.GetId());
+            sugestao.data = DateTime.Now;
+            if (ModelState.IsValid)
+            {
+                db.Sugestao.Add(sugestao);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.id_user = new SelectList(db.Utilizador, "id", "id_user", sugestao.id_user);
+            return View(sugestao);
+        }
+
+        // GET: Sugestaos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sugestao sugestao = db.Sugestao.Find(id);
+            if (sugestao == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.id_user = new SelectList(db.Utilizador, "id", "cod_postal", sugestao.id_user);
+            return View(sugestao);
+        }
+
+        // POST: Sugestaos/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id_user,data,descricao")] Sugestao sugestao)
+        {
+            if (ModelState.IsValid)
+            {
+                //db.Entry(sugestao).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.id_user = new SelectList(db.Utilizador, "id", "cod_postal", sugestao.id_user);
+            return View(sugestao);
+        }
+
+        // GET: Sugestaos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sugestao sugestao = db.Sugestao.Find(id);
+            if (sugestao == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sugestao);
+        }
+
+        // POST: Sugestaos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Sugestao sugestao = db.Sugestao.Find(id);
+            db.Sugestao.Remove(sugestao);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}

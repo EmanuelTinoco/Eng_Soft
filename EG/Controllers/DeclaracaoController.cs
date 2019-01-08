dac8ccf3b04e5f7b9a685c195928f3219b5c1912
Rui Meira <ruimiguelcm96@gmail.com>
2019-01-08 23:20:12 +0000

    hsahkaskj

---
+++ EG/Controllers/DeclaracaoController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BD;
+
+namespace EG.Controllers
+{
+    public class DeclaracaoController : Controller
+    {
+
+        private estp2Entities db = new estp2Entities();
+        Declaracao d;
+        // GET: Declaracao
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

e793707afa7d0ba64be0ac1c773ca4a74e85193f
DESKTOP-C3S6S3K\Emanuel Tinoco <emanuel.g.tinoco@gmail.com>
2019-01-12 19:38:50 +0000

    uppp

--- API_ISI/Controllers/RegistoController.cs
+++ API_ISI/Controllers/RegistoController.cs
@@ -30,38 +30,80 @@ namespace API_ISI.Controllers
             con = new SqlConnection(constr);
         }
 
-        [System.Web.Http.HttpGet]
-        [System.Web.Http.ActionName("Registo")]
-        public Registo Get(int id)
-        {
-            //return listEmp.First(e => e.ID == id);  
-            SqlDataReader reader = null;
+        //[System.Web.Http.HttpGet]
+        //[System.Web.Http.ActionName("Registo")]
+        //public List<Registo> Get(int id)
+        //{
+        //    //return listEmp.First(e => e.ID == id);  
+        //    SqlDataReader reader = null;
 
+        //    string constr = ConfigurationManager.ConnectionStrings["registoatividade"].ToString();
+        //    con = new SqlConnection(constr);
+
+        //    com = new SqlCommand("ShowRegistos", con);
+        //    com.CommandType = CommandType.StoredProcedure;
+
+        //    com.Parameters.AddWithValue("@id", id);
+
+        //    con.Open();
+        //    reader = com.ExecuteReader();
+
+        //    List<Registo> Lista = new List<Registo>();
+        //    Registo emp = null;
+        //    while (reader.Read())
+        //    {
+        //        emp = new Registo();
+        //        emp.Id = Convert.ToInt32(reader.GetValue(0));
+        //        emp.Data_Hora_Login = DateTime.Parse(reader.GetValue(1).ToString());
+        //        emp.Data_Hora_Logoff = DateTime.Parse(reader.GetValue(2).ToString());
+        //        Lista.Add(emp);
+        //    }
+        //    con.Close();
+        //    return Lista;           
+        //}
+
+
+        [System.Web.Http.HttpPost]
+        [System.Web.Http.Route("EliminaRegisto")]
+        public void Delete(int id)
+        {
+            
+            //return listEmp.First(e => e.ID == id);  
             string constr = ConfigurationManager.ConnectionStrings["registoatividade"].ToString();
             con = new SqlConnection(constr);
 
-            com = new SqlCommand("ShowRegistos", con);
+            com = new SqlCommand("DeleteRegisto", con);
             com.CommandType = CommandType.StoredProcedure;
 
             com.Parameters.AddWithValue("@id", id);
 
-            //SqlCommand sqlCmd = new SqlCommand();
-            //sqlCmd.CommandType = CommandType.Text;
-            //sqlCmd.CommandText = "Select * from registo where Userid=" + id;
-            //sqlCmd.Connection = con;
-            con.Open();
-            reader = com.ExecuteReader();
 
-            Registo emp = null;
-            while (reader.Read())
-            {
-                emp = new Registo();
-                emp.Id = Convert.ToInt32(reader.GetValue(0));
-                emp.Data_Hora_Login = DateTime.Parse(reader.GetValue(1).ToString());
-                emp.Data_Hora_Logoff = DateTime.Parse(reader.GetValue(2).ToString());
-            }
+            con.Open();
+            int n = com.ExecuteNonQuery();
             con.Close();
-            return emp;           
+            if (n>0)
+            {
+               
+              //  return true;
+                
+            }
+
+            //return false;
+
+            //reader = com.ExecuteReader();
+
+            //List<Registo> Lista = new List<Registo>();
+            //Registo emp = null;
+            //while (reader.Read())
+            //{
+            //    emp = new Registo();
+            //    emp.Id = Convert.ToInt32(reader.GetValue(0));
+            //    emp.Data_Hora_Login = DateTime.Parse(reader.GetValue(1).ToString());
+            //    emp.Data_Hora_Logoff = DateTime.Parse(reader.GetValue(2).ToString());
+            //    Lista.Add(emp);
+            //}
+            //con.Close();
+            //return Lista;
         }
 
     }
